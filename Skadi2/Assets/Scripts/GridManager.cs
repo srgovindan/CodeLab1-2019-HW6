a57c580700158b5313b661bf7f6b10b0555f0c2e
Siddarth <siddarthrgovindan@gmail.com>
2019-03-31 15:56:37 -0400

    Created GridManager.cs to load levels from ascii files

---
+++ Skadi2/Assets/Scripts/GridManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GridManager : MonoBehaviour
+{
+
+    public static GridManager GM;
+    public int levelNum;
+    
+    [Header("Tile Prefabs")] 
+    public GameObject iceTile;
+    public GameObject startTile;
+    public GameObject goalTile;
+    [Header("Player Prefab")] 
+    public GameObject player;
+    
+    void Start()
+    {
+        // SINGLETON
+        if (GM == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            GM = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        //Load first level
+        levelNum = 1;
+        LoadLevel(1);
+    }
+    
+    /// <summary>
+    /// Takes an int number input and loads the corresponding level from a text file.
+    /// </summary>
+    public void LoadLevel(int levelIndex)
+    {
+        // hold the level file path in a string
+        string filePath = Application.dataPath + "/level" + levelIndex + ".txt";
+        // if the level file does not exist, create it
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "X");
+        }
+        // reading from file
+        string[] inputLines = File.ReadAllLines(filePath);
+        for (int y = 0; y < inputLines.Length; y++)
+        {
+            string line = inputLines[y];
+            for (int x = 0; x < line.Length; x++)
+            {
+                GameObject tile;
+                GameObject playerObject;
+                //create tiles based on ascii
+                switch (line[x])
+                {
+                    case 'S':
+                        tile = Instantiate(startTile);
+                        tile.GetComponent<GoalTile>();
+                        tile.transform.position = new Vector3(x - line.Length / 2f,0f, inputLines.Length / 2f - y);
+                        break;
+                    case 'x':
+                        tile = Instantiate(iceTile);
+                        tile.GetComponent<IceTile>();
+                        tile.transform.position = new Vector3(x - line.Length / 2f,0f, inputLines.Length / 2f - y);
+                        break;
+                    case 'G':
+                        tile = Instantiate(goalTile);
+                        tile.GetComponent<GoalTile>();
+                        tile.transform.position = new Vector3(x - line.Length / 2f,0f, inputLines.Length / 2f - y);
+                        break;
+                    case 'P':
+                        tile = Instantiate(startTile);
+                        tile.GetComponent<StartTile>();
+                        tile.transform.position = new Vector3(x - line.Length / 2f,0f, inputLines.Length / 2f - y);
+                        //create and set player pos
+                        playerObject = Instantiate(player);
+                        playerObject.transform.position =  new Vector3(x - line.Length / 2f,.5f, inputLines.Length / 2f - y);
+                        break;
+                }
+            }
+        }
+    }
+    
+}
---
+++ Skadi2/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    public int x, y;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
