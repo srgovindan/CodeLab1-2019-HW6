4ff4afa74d13496ff2fcf0386a05b9cbe61bde96
Siddarth <siddarthrgovindan@gmail.com>
2019-03-31 03:03:00 -0400

    Created Ice Tile prefab Created TileManager and Tile class

---
+++ Skadi/Assets/Scripts/TileManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    public static TileManager instance;
+    
+    public Tile[][] Grid;
+    public int GridSizeX;
+    public int GridSizeY;
+
+    [Header("Tile Prefabs")] 
+    public GameObject iceTile;
+    public GameObject iceTileCracked;
+    public GameObject goalTile;
+    public GameObject wallTile;
+    
+    void Start()
+    {
+        // SINGLETON
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+       CreateGrid();
+    }
+
+    void CreateGrid()
+    { 
+        // Initialize Grid Array
+        Grid = new Tile[GridSizeX][];
+        Debug.Log(Grid);
+        Debug.Log(Grid[0]);
+       // for Grid.
+        
+        // Create grid of tiles
+        for (int i = 0; i < GridSizeX; i++)
+        {
+            for (int j = 0; j < GridSizeY; j++)
+            {
+                // Create a tile at x,y 
+                Grid[i][j] = CreateTile(i, j);
+            }
+        } 
+    }
+
+    Tile CreateTile(int x, int y)
+    {
+        Tile newTile = new Tile();
+        newTile.x = x;
+        newTile.y = y;
+    
+        newTile.tileObject = Instantiate(iceTile);
+        newTile.tileObject.transform.position = new Vector3(x*1f,0f, y*1f);
+        return newTile;
+    }
+    
+
+    
+    //TODO: Write a function to see if all Ice Tiles are cracked 
+}
