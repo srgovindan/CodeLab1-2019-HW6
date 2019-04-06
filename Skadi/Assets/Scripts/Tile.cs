76eafd4e40ef32095af6d4b79466b4442f85262f
Siddarth <siddarthrgovindan@gmail.com>
2019-04-06 00:46:39 -0400

    TODO: Consolidate Cell and Tile class

--- Skadi/Assets/Scripts/Tile.cs
+++ Skadi/Assets/Scripts/Tile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    //TODO: Consolidate Tile and Cell class
     public int x, y;
     // Start is called before the first frame update
     void Start()
